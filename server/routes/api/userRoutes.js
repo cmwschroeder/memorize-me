f000180482d223eb6dde28cddb681148e616554c
cmwschroeder <cmwschroeder@gmail.com>
2022-06-04 11:31:38 -0700

    added sending token back when creating user, created login route that will check user in database and send a token back with user if email and password were correct

--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -6,9 +6,11 @@ const {
     getSingleUser,
     updateUser,
     deleteUser,
+    login,
 } = require('../../controllers/userControllers')
 
 router.route('/').get(getUsers).post(createUser);
 router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/login').post(login);
 
 module.exports = router;
