1da5ebe8d6c0f6277160a3ce9d1f6370ae777ae2
cmwschroeder <cmwschroeder@gmail.com>
2022-06-05 14:52:09 -0700

    created start of react app, created a navbar, styled everything with tailwind and daisyui

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="bg-base-200 min-h-screen">
+            <p>This is the home page</p>
+        </div>
+    )
+}
+
+export default Home; 
