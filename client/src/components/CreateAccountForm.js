d42a869910cbb0e24275a3f958b2313186c4f258
cmwschroeder <cmwschroeder@gmail.com>
2022-06-05 21:20:07 -0700

    added telling user that inputs are required, added email validation and password length checking

--- client/src/components/CreateAccountForm.js
+++ client/src/components/CreateAccountForm.js
@@ -1,6 +1,68 @@
 import React from 'react';
 
 function CreateAccountForm({ switchForm }) {
+
+    const checkEmptyUsername = function (e) {
+        if (e.target.value === '') {
+            e.target.setAttribute('placeholder', 'Username cannot be blank');
+            e.target.classList.add('input-error');
+        }
+    }
+
+    const checkEmptyEmail = function (e) {
+        if (e.target.value === '') {
+            e.target.setAttribute('placeholder', 'Email cannot be blank');
+            e.target.classList.add('input-error');
+        }
+    }
+
+    const checkEmptyPassword = function (e) {
+        if (e.target.value === '') {
+            e.target.setAttribute('placeholder', 'Password cannot be blank');
+            e.target.classList.add('input-error');
+        }
+    }
+
+    const setPlaceholderUsername = function (e) {
+        e.target.setAttribute('placeholder', 'Username');
+        e.target.classList.remove('input-error');
+    }
+
+    const setPlaceholderEmail = function (e) {
+        e.target.setAttribute('placeholder', 'Email');
+        e.target.classList.remove('input-error');
+    }
+
+    const setPlaceholderPassword = function (e) {
+        e.target.setAttribute('placeholder', 'Password');
+        e.target.classList.remove('input-error');
+    }
+
+    const checkValidEmail = function (e) {
+        var emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if (e.target.value.match(emailRegex)) {
+            if (!document.getElementById('incorrectEmail').classList.contains('hidden')) {
+                document.getElementById('incorrectEmail').classList.add('hidden');
+            };
+        } else {
+            if (document.getElementById('incorrectEmail').classList.contains('hidden')) {
+                document.getElementById('incorrectEmail').classList.remove('hidden');
+            };
+        }
+    }
+
+    const checkValidPassword = function (e) {
+        if (e.target.value.length >= 8) {
+            if (!document.getElementById('incorrectPassword').classList.contains('hidden')) {
+                document.getElementById('incorrectPassword').classList.add('hidden');
+            };
+        } else {
+            if (document.getElementById('incorrectPassword').classList.contains('hidden')) {
+                document.getElementById('incorrectPassword').classList.remove('hidden');
+            };
+        }
+    }
+
     return (
         <div id="createAccount" className="w-full flex justify-center">
             <div className="card w-1/3 bg-base-100 shadow-xl my-6 p-3">
@@ -12,7 +74,7 @@ function CreateAccountForm({ switchForm }) {
                             <span className="label-text text-2xl text-primary">Username: </span>
                         </label>
                         <label className="input-group">
-                            <input type="text" placeholder="Username" className="input input-bordered w-full" id="create-username" />
+                            <input type="text" placeholder="Username" className="input input-bordered w-full" id="create-username" onBlur={checkEmptyUsername} onFocus={setPlaceholderUsername} />
                         </label>
                     </div>
 
@@ -21,8 +83,9 @@ function CreateAccountForm({ switchForm }) {
                             <span className="label-text text-2xl text-primary">Email: </span>
                         </label>
                         <label className="input-group">
-                            <input type="text" placeholder="Email" className="input input-bordered w-full" id="create-email" />
+                            <input type="text" placeholder="Email" className="input input-bordered w-full" id="create-email" onBlur={checkEmptyEmail} onFocus={setPlaceholderEmail} onKeyUp={checkValidEmail} />
                         </label>
+                        <p className='my-2 hidden text-error' id='incorrectEmail'>Incorrect Email Format</p>
                     </div>
 
                     <div className="form-control w-5/6 mx-auto">
@@ -30,9 +93,9 @@ function CreateAccountForm({ switchForm }) {
                             <span className="label-text text-2xl text-primary">Password: </span>
                         </label>
                         <label className="input-group">
-                            <input type="password" placeholder="Password" className="input input-bordered w-full" id="create-password" />
+                            <input type="password" placeholder="Password" className="input input-bordered w-full" id="create-password" onBlur={checkEmptyPassword} onFocus={setPlaceholderPassword} onKeyUp={checkValidPassword} />
                         </label>
-                        <p className='my-2 hidden text-error' id='incorrectEmail'>Incorrect Email Format</p>
+                        <p className='my-2 hidden text-error' id='incorrectPassword'>Password must be at least 8 characters long</p>
                     </div>
                 </div>
                 <div className="w-5/6 mx-auto">
--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,56 @@
 import React from 'react';
 
 function LoginForm({ switchForm }) {
+
+    const checkEmptyEmail = function (e) {
+        if (e.target.value === '') {
+            e.target.setAttribute('placeholder', 'Email cannot be blank');
+            e.target.classList.add('input-error');
+        }
+    }
+
+    const checkEmptyPassword = function (e) {
+        if (e.target.value === '') {
+            e.target.setAttribute('placeholder', 'Password cannot be blank');
+            e.target.classList.add('input-error');
+        }
+    }
+
+    const setPlaceholderEmail = function (e) {
+        e.target.setAttribute('placeholder', 'Email');
+        e.target.classList.remove('input-error');
+    }
+
+    const setPlaceholderPassword = function (e) {
+        e.target.setAttribute('placeholder', 'Password');
+        e.target.classList.remove('input-error');
+    }
+
+    const checkValidEmail = function (e) {
+        var emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+        if (e.target.value.match(emailRegex)) {
+            if (!document.getElementById('incorrectEmail').classList.contains('hidden')) {
+                document.getElementById('incorrectEmail').classList.add('hidden');
+            };
+        } else {
+            if (document.getElementById('incorrectEmail').classList.contains('hidden')) {
+                document.getElementById('incorrectEmail').classList.remove('hidden');
+            };
+        }
+    }
+
+    const checkValidPassword = function (e) {
+        if (e.target.value.length >= 8) {
+            if (!document.getElementById('incorrectPassword').classList.contains('hidden')) {
+                document.getElementById('incorrectPassword').classList.add('hidden');
+            };
+        } else {
+            if (document.getElementById('incorrectPassword').classList.contains('hidden')) {
+                document.getElementById('incorrectPassword').classList.remove('hidden');
+            };
+        }
+    }
+
     return (
         <div id="login" className="w-full flex justify-center">
             <div className="card w-1/3 bg-base-100 shadow-xl my-6 p-3">
@@ -12,8 +62,9 @@ function LoginForm({ switchForm }) {
                             <span className="label-text text-2xl text-primary">Email: </span>
                         </label>
                         <label className="input-group">
-                            <input type="text" placeholder="Email" className="input input-bordered w-full" id="email" />
+                            <input type="text" placeholder="Email" className="input input-bordered w-full" id="email" onBlur={checkEmptyEmail} onFocus={setPlaceholderEmail} onKeyUp={checkValidEmail} />
                         </label>
+                        <p className='my-2 hidden text-error' id='incorrectEmail'>Incorrect Email Format</p>
                     </div>
 
                     <div className="form-control w-5/6 mx-auto">
@@ -21,9 +72,9 @@ function LoginForm({ switchForm }) {
                             <span className="label-text text-2xl text-primary">Password: </span>
                         </label>
                         <label className="input-group">
-                            <input type="password" placeholder="Password" className="input input-bordered w-full" id="password" />
+                            <input type="password" placeholder="Password" className="input input-bordered w-full" id="password" onBlur={checkEmptyPassword} onFocus={setPlaceholderPassword} onKeyUp={checkValidPassword} />
                         </label>
-                        <p className='my-2 hidden text-error' id='incorrectEmail'>Incorrect Email Format</p>
+                        <p className='my-2 hidden text-error' id='incorrectPassword'>Password must be at least 8 characters long</p>
                     </div>
                 </div>
                 <div className="w-5/6 mx-auto">
