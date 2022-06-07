aa71d4c9122732e3c016f5bb745513883c4e7036
akaur091 <amniderkaur.09@gmail.com>
2022-06-06 21:02:30 -0700

    initial psuedocode and begin creating the funcitons

---
+++ client/src/pages/NumberGame.js
@@ -0,0 +1,29 @@
+// import react for use context.
+import React, {useContext} from "react";
+// import app context
+import App from "../App";
+// create a header
+export default function header(props) {
+    return (
+        <div> 
+            <h2>Memorize the Numbers</h2>
+        </div> 
+    )
+};
+// set difficulty (easy, medium, hard)
+    // export default function 
+    // cosnt use context in respect to app context
+    // return html code: 
+        // direction tag
+        // list of options for easy medium or hard
+        // make them buttons
+
+// create a function that ranomdizes numbers to be displayed
+    // add a timer that will make the number disapper after a certain amount of time
+    // add text box for answer submissom
+
+// create a function that checks if teh answer is incorrect
+    // if correct then add point and got to next number
+    // if incorrect (take time from timer or keep going)
+// create a timer that times the
+// create a funtion that takes in the high score and add to the highscores page
