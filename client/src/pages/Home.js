e7ae0fa0f06a631b624b87e1ecb8f6f903992ba5
walterunderwood <walonsounderwood@gmail.com>
2022-06-07 23:10:06 -0700

    Creating Hero on Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
 function Home() {
-
     const [games, setGames] = useState([]);
 
     useEffect(() => {
@@ -26,24 +25,36 @@ function Home() {
     }
     else {
         return (
-            <div className="min-h-screen w-full bg-base-200 ">
-                <div className="w-5/6 mx-auto flex flex-row flex-wrap justify-around">
-                    {games.map((game) => {
-                        return (
-                            <div className="card bg-base-100 shadow-xl w-2/5 m-5">
-                                <figure className="h-96"><img src={"./preview-images/" + game.image} alt="Game Preview" /></figure>
-                                <div className="card-body w-full">
-                                    <h2 className="card-title">{game.title}</h2>
-                                    <p>{game.description}</p>
-                                    <div className="card-actions justify-end mt-8">
-                                        <a href={'/game/' + (game._id)} className="btn btn-primary w-1/2">Play</a>
+            <>
+                <div class="hero min-h-screen bg-base-200">
+                    <div class="hero-content flex-col lg:flex-row-reverse">
+                        <img src="https://user-images.githubusercontent.com/99919050/172543555-0eeb4525-9c2a-47b6-abe2-4c1ddb18b2d5.png" class="max-w-sm rounded-lg shadow-2xl" />
+                        <div>
+                            <h1 class="text-5xl font-bold myscores">Memorize.Me</h1>
+                            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                            <a class="btn btn-primary gamecards" href="/login" >Get Started</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="min-h-screen w-full bg-base-200 ">
+                    <div className="w-5/6 mx-auto flex flex-row flex-wrap justify-around ">
+                        {games.map((game) => {
+                            return (
+                                <div className="card w-96 glass shadow-xl w-1/3 m-5 gamecards">
+                                    <figure className="h-96"><img src={"./preview-images/" + game.image} alt="Game Preview" /></figure>
+                                    <div className="card-body w-full">
+                                        <h2 className="card-title">{game.title}</h2>
+                                        <p>{game.description}</p>
+                                        <div className="card-actions justify-end">
+                                            <a href={'/game/' + (game._id)} className="btn btn-primary w-1/2" >Play</a>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
-                    })}
+                            );
+                        })}
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
