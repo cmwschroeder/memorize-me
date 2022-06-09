59777d7e954c724475bdf46ba8087a0a12cc8e14
akaur091 <amniderkaur.09@gmail.com>
2022-06-09 11:46:39 -0700

    redo math fucntion to generate numbers

--- client/src/pages/NumberGame.js
+++ client/src/pages/NumberGame.js
@@ -1,82 +1,35 @@
 // import react for use context.
 import React, {useEffect, useState} from "react";
 // import app context
-import App from "../App";
-// create a header
-export default function header(props) {
-    return (
-        <div> 
-            <h2>Memorize the Numbers</h2>
-            <button onClick={start}>Start Game</button>
-        </div> 
-    )
-};
-  
 // }
 //  redo code
 // create a function that ranomdizes numbers to be displayed
-export default function NumberGame(props) {
-    const App = () => {
-        const [num , setNum] = useState(0);
-        function generateRandomNum (min, max){
-            return Math.floor(Math.random() * (max - min +1)) + min;
+ function NumberGame(props) {
+    const [randomNumber , setRandomNumber] = useState("");
+    const chooseRandomNumber = (numberLength) => {
+        let number = '';
+        for (let i = 0; i < numberLength; i ++){
+            number = number + (Math.floor(Math.random()* 10)).toString();
         }
-        useEffect(() => {
-            const interval = setInterval(() => {
-                setNum(generateRandomNum (0,10));
-            }, 2000);
-            return() => {
-                clearInterval(interval);
-            }
-        }, [])
+        setRandomNumber(number);
+        return number;
     }
-    let {submitAnswer, typedAnswer, setTypedNum,} = value;
-    // create seperate functions for increasing digits
-    // 1 digit
-    Math.floor(Math.random()* 10) + 1 ;
-    // 2 digits
-    function twoDigits () {
-        var min = 10;
-        var max = 100;
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
-    // 3 digits
-    function threeDigits () {
-        var min = 100;
-        var max = 1000;
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
-    // 4 digits
-    function fourDigits () {
-        var min = 1000;
-        var max = 10000;
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
-    // 5 digits
-    function fiveDigits() {
-        var min = 10000;
-        var max = 100000;
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
-    // 6 digits
-    function sixDigits() {
-        var min = 100000;
-        var max = 1000000;
-        return Math.floor(Math.random() * (max - min) ) + min;
-    }
-
     return (
         <div>
             <h7>Submit your answer</h7> 
-            <form onSubmit={submitAnswer}>
-                <textarea placeholder="type answer here" onChange={(n) => setTypedNum(n.target.value)} value ={typedAnswer}></textarea>
+            <form>
+                <textarea placeholder="type answer here"></textarea>
                 <button type="submit">Submit answer</button>
             </form>
+            <div>
+                <p>{randomNumber}</p>
+            </div>
         </div>
        
     );
 
 }
+export default NumberGame;
     // add a timer that will make the number disapper after a certain amount of time
     // add text box for answer submissom
 
@@ -87,3 +40,38 @@ export default function NumberGame(props) {
 // create a funtion that takes in the high score and add to the highscores page
     // create button to save highscore and it will go to high score page
     // create a play again button
+
+        // // create seperate functions for increasing digits
+    // // 1 digit
+    // Math.floor(Math.random()* 10) + 1 ;
+    // // 2 digits
+    // function twoDigits () {
+    //     var min = 10;
+    //     var max = 100;
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
+    // // 3 digits
+    // function threeDigits () {
+    //     var min = 100;
+    //     var max = 1000;
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
+    // // 4 digits
+    // function fourDigits () {
+    //     var min = 1000;
+    //     var max = 10000;
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
+    // // 5 digits
+    // function fiveDigits() {
+    //     var min = 10000;
+    //     var max = 100000;
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
+    // // 6 digits
+    // function sixDigits() {
+    //     var min = 100000;
+    //     var max = 1000000;
+    //     return Math.floor(Math.random() * (max - min) ) + min;
+    // }
+    
